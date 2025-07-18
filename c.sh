57ee72d6d65135dfebc97706d21670a06dc19c5f
Rui Ribeiro <ruyrybeyro@gmail.com>
2025-07-18 13:24:14 +0100

    compatibility with Makefile

--- c.sh
+++ c.sh
@@ -3,5 +3,7 @@
 pasmo -o - src/Z80FC.asm | od -An -t u1 | tr -s ' ' ',' | sed 's/^,//' | sed 's/,$//'
 # create TAP
 zmakebas -a10 -o dist/Z80FC.tap src/Z80FC.bas
+# Just for compatibility with Makefile
+cp dist/Z80FC.tap build/Z80FC.tap
 # create TZX
 tap2tzx dist/Z80FC.tap
