8cae43e24b30592a9d848fbb1a02308233710b50
Rui Ribeiro <ruyrybeyro@gmail.com>
2025-07-18 02:44:01 +0100

    AI suggested improvements

--- c.sh
+++ c.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # show decimal disassembly
-pasmo -o - Z80FC.asm | od -An -t u1 | tr -s ' ' ',' | sed 's/^,//' | sed 's/,$//'
+pasmo -o - src/Z80FC.asm | od -An -t u1 | tr -s ' ' ',' | sed 's/^,//' | sed 's/,$//'
 # create TAP
-zmakebas -a10 -o Z80FC.tap Z80FC.bas
+zmakebas -a10 -o dist/Z80FC.tap src/Z80FC.bas
 # create TZX
-tap2tzx Z80FC.tap
+tap2tzx dist/Z80FC.tap
